5c5a629d977ec7193ef00a14f8fa99eefe6d799d
HectorAlonso18 <65554952+HectorAlonso18@users.noreply.github.com>
2022-08-19 20:54:08 -0500

    Add files via upload

--- include/Purepursuit.h
+++ include/Purepursuit.h
@@ -3,11 +3,11 @@
 #include <vector>
 
 int sign(double x);
-float point_line_distance(std::vector<double> p1, std::vector<double> p2, std::vector<double> p3);
+
 double distance(std::vector<double> p1, std::vector<double> p2);
 double get_degrees(std::vector<double> p1, std::vector<double> p2);
-std::vector<double> get_deviation(std::vector<double> headings, int numb);
-std::vector<double> get_intersection(std::vector<double> start, std::vector<double> end, std::vector<double> cur, double radius);
+
+std::vector<double> get_intersection(std::vector<double> start, std::vector<double> end, std::vector<double> cur, double lookAheadDis);
 
 
 
