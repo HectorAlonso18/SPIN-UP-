cafc821fb73b4b699486b598596f27f8df99d59e
HectorAlonso18 <65554952+HectorAlonso18@users.noreply.github.com>
2022-11-24 18:30:32 -0600

    Add files via upload

--- src/main.cpp
+++ src/main.cpp
@@ -22,60 +22,73 @@ void competition_initialize() {}
 void autonomous() {
 	Robot::start_task("TRACKING", Robot::raestro);
     
+		
 	//Robot va por el primer disco y gira el roller. 
   
   
 	Robot::eat(true);
 	//El robot toma el primer disco
-	Robot::Odom_Movement(fuctPtr_move_to,1, {-1.22,8.5,318}, Drive_Constant,Turn_Constant, 3, 0,0,0);
-	pros::delay(1000);
-	Robot::Odom_Movement(fuctPtr_move_to,1, {4.5,5.5,0}, Drive_Constant,Turn_Constant, 3, 0,0,0);	
-	Robot::Odom_Movement(fuctPtr_move_to,1, {15,5.5,90}, Drive_Constant,Turn_Constant, 3, 0,0,0);
+	//Robot::Odom_Movement(fuctPtr_move_to,1, {1.8,6.8,316}, Drive_Constant,Turn_Constant, 3, 0,0,0);
+	
+	Robot::Odom_Movement(fuctPtr_move_to,1, {1.8,8,316}, Drive_Constant,Turn_Constant, 3, 0,0,0);
+    pros::delay(500); 
+    Robot::Odom_Movement(fuctPtr_move_to,1, {10,6,90}, Drive_Constant,Turn_Constant, 3, 0,0,0);
+	
 
 
-    //El robot se acomoda para girar el roller
-	Robot::Odom_Movement(fuctPtr_move_to,1, {-2.31,5.8,180}, Drive_Constant,Turn_Constant, 1,5, 0,0);
-	pros::delay(500);
+
 	Robot::eat(false);
 	//Se acerca al roller
-	Robot::Odom_Movement(fuctPtr_move_to, 1,{-4,3.5,180}, Drive_Constant,Turn_Constant, 3, 0,0,0);
+	Robot::Odom_Movement(fuctPtr_move_to, 1,{-8.5,0.5,180}, Drive_Constant,Turn_Constant, 2, 0,0,0);
 	//Gira Roller
-	Robot::Move_Roller(-338, 180);
-	
+	Robot::Move_Roller(338, 180);
+    
+	pros::delay(500); 
+    
+	Robot::Odom_Movement(fuctPtr_move_to, 1,{-8.5,6,180}, Drive_Constant,Turn_Constant, 1, 0,0,0);
 
-	//Robot realiza el primer disparo y mete los 3 discos
+	//5
+	Robot::Odom_Movement(fuctPtr_move_to, 1,{8,8,157}, Drive_Constant,Turn_Constant, 3, 0,0,0);
 
-	pros::delay(500);
-	Robot::Odom_Movement(fuctPtr_move_to,1, {6,17,178}, Drive_Constant,Turn_Constant, 3, 0,0,0);	
-
+	//Realiza tiro
 	pros::delay(10); 
-    Robot::Flywheel_pid_shoot(2860, {.01535,.00000525,4.05}, 3,0);
+    Robot::Flywheel_pid_shoot(2890, {.01535,.00000525,4.05}, 3,125);
+	pros::delay(100); 
 
+  
+    
+	//IR A LA ESCUADRA 
+////////////////////////////////////////////////////////////////////////////////////////////
+	/*
+	Robot::eat(true);
  
 
-	//Va hacia la escuadra
-	Robot::eat(true);
-	Robot::Odom_Movement(fuctPtr_move_to, 1,{40,0,90}, Drive_Constant,Turn_Constant, 1, 0,0,0);
-	Robot::Odom_Movement(fuctPtr_move_to, 1,{46,17,90}, Drive_Constant,Turn_Constant, 3, 0,0,0);
-	Robot::Odom_Movement(fuctPtr_move_to, 1,{59,17,90}, Drive_Constant,Turn_Constant, 3, 0,0,0);
-	Robot::Odom_Movement(fuctPtr_move_to, 1,{46,17,90}, Drive_Constant,Turn_Constant, 3, 0,0,0);
-	Robot::Odom_Movement(fuctPtr_move_to, 1,{46,27.5,90}, Drive_Constant,Turn_Constant, 3, 0,0,0);
-	Robot::Odom_Movement(fuctPtr_move_to, 1,{59,27.5,90}, Drive_Constant,Turn_Constant, 3, 0,0,0);
-	Robot::Odom_Movement(fuctPtr_move_to, 1,{46,27.5,90}, Drive_Constant,Turn_Constant, 3, 0,0,0);
-	Robot::Odom_Movement(fuctPtr_move_to, 1,{46,40,90}, Drive_Constant,Turn_Constant, 3, 0,0,0);
-	Robot::Odom_Movement(fuctPtr_move_to, 1,{61,40,90}, Drive_Constant,Turn_Constant, 3, 0,0,0);
-	Robot::Odom_Movement(fuctPtr_move_to, 1,{46,40,90}, Drive_Constant,Turn_Constant, 3, 0,0,0);
+	Robot::Odom_Movement(fuctPtr_move_to, 1, {24,10,90}, Drive_Constant, Turn_Constant, 3, 0, 0, 0);
+
+	//Agarra el primer disco 
+	Robot::Odom_Movement(fuctPtr_move_to, 1, {40,16.5,90}, Drive_Constant, Turn_Constant, 3, 0, 0, 0);
+
+    //Retrocede
+	Robot::Odom_Movement(fuctPtr_move_to, 1, {28,17,90}, Drive_Constant, Turn_Constant, 3, 0, 0, 0);
+ 
+	//Prepara para el segundo disco
+    Robot::Odom_Movement(fuctPtr_move_to, 1, {28,30,90}, Drive_Constant, Turn_Constant, 3, 0, 0, 0);
+
+	//Agarra el segundo disco
+	Robot::Odom_Movement(fuctPtr_move_to, 1, {40,30,90}, Drive_Constant, Turn_Constant, 3, 0, 0, 0);
+
+	//Va por el último disco
+	Robot::Odom_Movement(fuctPtr_move_to, 1, {28,30,90}, Drive_Constant, Turn_Constant, 3, 0, 0, 0);
+
+	Robot::Odom_Movement(fuctPtr_move_to, 1, {28,43,90}, Drive_Constant, Turn_Constant, 3, 0, 0, 0);
+
+	Robot::Odom_Movement(fuctPtr_move_to, 1, {40,43,90}, Drive_Constant, Turn_Constant, 3, 0, 0, 0);
+
+	Robot::Odom_Movement(fuctPtr_move_to, 1, {28,43,157}, Drive_Constant, Turn_Constant, 3, 0, 0, 0);
+
+ 	Robot::Flywheel_pid_shoot(2700, {.01535,.00000525,4.05}, 3,150);
+*/
 	
-	Robot::Odom_Movement(fuctPtr_move_to, 1,{43,41,152.5}, Drive_Constant,Turn_Constant, 3, 0,0,0);
-    Robot::Odom_Movement(fuctPtr_move_to, 1,{43,42,152.5}, Drive_Constant,Turn_Constant, 3, 0,0,0);
-    pros::delay(100);
-	Robot::eat(false);
-	pros::delay(100);
-	Robot::Flywheel_pid_shoot(2100, {.01535,.00000525,4.05}, 3,1000);	
-
-
-
-
 
   
 
@@ -87,7 +100,7 @@ void autonomous() {
 void opcontrol() {
 
 	Robot::start_task("DRIVE", Robot::drive);
-    Robot::start_task("TRACKING", Robot::raestro);
+    //Robot::start_task("TRACKING", Robot::raestro);
     
    
 }
